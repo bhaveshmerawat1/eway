c61d09bf0868679a73d99d927af92ae0ca363409
Bhavesh Merawat <bhavesh.merawat@galaxyweblink.in>
2025-08-26 17:44:49 +0530

    minor changes

--- src/app/home/pages.tsx
+++ src/app/home/pages.tsx
@@ -12,7 +12,7 @@ import Button from '@/components/Button';
 export default function HomePage() {
   return (
     <>
-    <Header />
+      <Header />
       <BrandCarousel />
       <div id="ads-section" className='w-full my-4'>
         <div className='container mx-auto'>
@@ -21,7 +21,7 @@ export default function HomePage() {
               <Image alt='advertiment banner' src={images.adsImg1} className='w-full h-full object-position' fill
                 quality={100} />
             </a>
-            <a href='#'  className='w-full h-[300px] relative block'>
+            <a href='#' className='w-full h-[300px] relative block'>
               <Image alt='advertiment banner' src={images.adsImg2} className='w-full h-full object-position' fill
                 quality={100} />
             </a>
@@ -29,11 +29,11 @@ export default function HomePage() {
           <div className='w-full my-4 bg-red-100 py-8'>
             <h1 className='font-semibold text-[15px] text-black text-center mb-2'>Ready to shop?</h1>
             <p className='font-medium text-[17px] text-black text-center mb-2 tracking-[0.3px]'>Sign in or shop as a guest to view prices and product availability.</p>
-          <div className='flex justify-center mt-4 items-center gap-4'>
-              <button children={"Sign In"} onClick={() => { }} className='md:rounded-3xl bg-red-600 text-white md:py-2 md:px-6 border border-red-600 hover:bg-white hover:text-red-600 md:text-[14px] transition-all 
-							duration-400 ease-in-out font-myfont font-medium' />
-              <Button children={"Shop as Guest"} onClick={() => { }} className='md:rounded-3xl md:py-2 md:px-6 md:text-[14px] md:font-medium' />
-          </div>
+            <div className='flex justify-center mt-4 items-center gap-4'>
+              <button className='md:rounded-3xl bg-red-600 text-white md:py-2 md:px-6 border border-red-600 hover:bg-white hover:text-red-600 md:text-[14px] transition-all 
+							duration-400 ease-in-out font-myfont font-medium'> Sign In</button>
+              <Button children={"Shop as Guest"} className='md:rounded-3xl md:py-2 md:px-6 md:text-[14px] md:font-medium' />
+            </div>
           </div>
         </div>
       </div>
